1c0eefb91927fcf791eb02f12950c6306c7dad54
Etienne Pham Do <62301839+majockster@users.noreply.github.com>
2021-05-03 16:25:35 -0400

    Added temperature data handling

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import WeatherInfo from './components/WeatherInfo';
+const API_KEY = 'd9089573dfa7d293dd8759932fe923e1';
 
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'; //Use https:// to make url valid
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [unitsSystem, setUnitsSystem] = useState('metric');//for celsius
+  useEffect(() => {
+    load(); //async function
+  }, []);
+
+  async function load(){//Ask location permission
+    try{
+      let {status} = await Location.requestForegroundPermissionsAsync();
+
+      if(status != 'granted'){
+        setErrorMessage('Location Access Needed to Run App !');
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync();
+
+
+      const {latitude, longitude} = location.coords; 
+
+      const weather_url = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${API_KEY}`;
+
+      const response = await fetch(weather_url);
+      //alert(`Latitude : ${latitude}, Longitude: ${longitude}`);
+      const result = await response.json();
+
+      if(response.ok){
+        setCurrentWeather(result);
+      }
+      else{
+        setErrorMessage(result.message);
+      }
+    }
+    catch(error){
+      setErrorMessage(error.message);
+    }
+  }
+
+  if(currentWeather){
+    const {main : {temp}} = currentWeather;/*Accessing 2 levels of deconstruction*/
+    return (//JSX expression
+      /*Div element, elements mapped to IOS/Android components*/
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <View style={styles.main}>
+          <WeatherInfo currentWeather={currentWeather}/> 
+        </View>
+      </View>//Custom components like weatherInfo used like html tags
+    );} 
+    else{
+      return (//JSX expression
+        /*Div element, elements mapped to IOS/Android components*/
+        <View style={styles.container}>
+          <Text>{errorMessage}</Text>
+          <StatusBar style="auto" />
+        </View>
+      );
+   }
 }
 
-const styles = StyleSheet.create({
-  container: {
+
+const styles = StyleSheet.create({ //Styles object
+  container: {//Not CSS
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  main: {
+    justifyContent: 'center',
+    flex: 1
+  }
 });
---
+++ utils/index.js
@@ -0,0 +1,5 @@
+export const colors = {
+    PRIMARY_COLOR: '#ff304f',
+    SECOND_COLOR: '#002651',
+    BORDER_COLOR: '#dbdbdb'
+}
